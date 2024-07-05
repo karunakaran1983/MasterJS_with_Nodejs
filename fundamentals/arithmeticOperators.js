12ad633aee31363623097d79c7e6ed224be6abef
Karunakaran Dhayanidhi <karunaitsurlife@gmail.com>
2024-07-05 13:23:31 +0200

    arithmetic operators

---
+++ fundamentals/arithmeticOperators.js
@@ -0,0 +1,20 @@
+// Division
+
+let a = 50
+let b = 5
+console.log('Quotient is ', a / b)
+console.log('Reminder is ', a % b)
+console.log('Power of a to b is ', a ** b)
+console.log('Increment Assignment ', a += 10)
+let c = 10
+console.log('Decrement Assignment ', c -= 5)
+let d = 5
+console.log('Multiplication Assignment ', d *= 5)
+let e = 25
+console.log('Division Assignment ', e /= 5)
+let f = 10
+f++
+console.log('Incremental Operator ', f)
+f = 10
+f--
+console.log('Incremental Operator ', f)
