7bffba2f8339b7cade0349806736cf67156499b4
Karunakaran Dhayanidhi <karunaitsurlife@gmail.com>
2024-07-05 13:46:01 +0200

    logical and comparison operators

---
+++ fundamentals/logical_comparison_operators.js
@@ -0,0 +1,16 @@
+// logical Operators
+let a = true
+let b = false
+console.log('Logical AND - a && b        => ', a && b)
+console.log('Logical OR  - a || b        => ', a || b)
+console.log('Logical NOT - !a            => ', !a)
+console.log('Logical AND & NOT - a && !b => ', a && !b)
+console.log('Logical OR & NOT - !a || !b => ', !a || !b)
+
+// Comparison Operators
+let x = 100
+let y = 200
+console.log('Comparison > - x > y        => ', x > y)
+console.log('Comparison < - x < y        => ', x < y)
+console.log('Comparison >= - x >= y      => ', x >= y)
+console.log('Comparison <= - x <= y      => ', x <= y)
