f3143ce5e464982223f44a615c574dd28d819920
Karunakaran Dhayanidhi <karunaitsurlife@gmail.com>
2024-07-05 16:04:10 +0200

    flexibility of funtions

---
+++ functions/flexibilityOfFunctions.js
@@ -0,0 +1,15 @@
+function addNumbers(num1, num2) {
+    console.log(num1)
+    console.log(num2)
+}
+
+console.log('Passing 2 arguments')
+addNumbers(10, 15)
+console.log('Passing 1 argument')
+addNumbers(10)
+console.log('Passing 0 argument')
+addNumbers()
+console.log('Passing more arguments')
+addNumbers(10, 15, 25)
+console.log('Passing last argument')
+addNumbers(undefined, 15)
