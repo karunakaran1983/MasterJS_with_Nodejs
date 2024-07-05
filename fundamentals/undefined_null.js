d7c8351a443e64a2313dc61c9da7c3945ebce675
Karunakaran Dhayanidhi <karunaitsurlife@gmail.com>
2024-07-05 13:09:25 +0200

    undefined_null

---
+++ fundamentals/undefined_null.js
@@ -0,0 +1,13 @@
+// undefined - Variable that doesnt has a value
+// undefined is a Type
+
+let newVar
+console.log(newVar)
+console.log(typeof(newVar))
+
+// null - Object that doesnt has a value
+// null is a value
+// A variable can be shown as Object by just assigning null to it
+let checkIfThisIsVariableOrObject = null
+console.log("checkIfThisIsVariableOrObject ", checkIfThisIsVariableOrObject)
+console.log("checkIfThisIsVariableOrObject ", typeof(checkIfThisIsVariableOrObject))
