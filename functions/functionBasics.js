9df5fbf739637cbbae378c605005adc529ebe6c3
Karunakaran Dhayanidhi <karunaitsurlife@gmail.com>
2024-07-05 15:52:59 +0200

    function basics

---
+++ functions/functionBasics.js
@@ -0,0 +1,18 @@
+/*
+Functions:
+    1. Block of code which performs specific tasks
+    2. Can be called multiple times
+*/
+
+// functions with arguments
+function addNumbers(num1, num2) {
+    console.log(num1 + num2)
+}
+
+addNumbers(2, 5)
+
+// functions without arguments
+display()
+function display() {
+    console.log("This is Karun")
+}
