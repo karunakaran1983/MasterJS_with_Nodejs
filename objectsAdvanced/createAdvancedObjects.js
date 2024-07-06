3e40dcda5e7d45a755186a9e5b23df2f35d38dee
Karunakaran Dhayanidhi <karunaitsurlife@gmail.com>
2024-07-06 10:24:45 +0200

    create advanced objects

---
+++ objectsAdvanced/createAdvancedObjects.js
@@ -0,0 +1,34 @@
+// Traditional way of creating objects
+const student1 = {
+    name: "karun",
+    age: 40,
+    city: "rotterdam",
+    country: "The Netherlands"
+}
+
+const student2 = {
+    name: "ramya",
+    age: 40,
+    city: "schiedam",
+    country: "The Netherlands"
+}
+
+console.log(student1)
+console.log(student2)
+
+// THIS IS INEFFICIENT WAY OF CREATING OBJECTS
+
+function createStudent(name, age, city, country) {
+    const student = {}
+    student["name"] = name
+    student["age"] = age
+    student["city"] = city
+    student["country"] = country
+    return student
+}
+
+const s1 = createStudent("yash", 32, "texas", "USA")
+const s2 = createStudent("gnanavel", 55, "pondy", "India")
+
+console.log(s1)
+console.log(s2)
