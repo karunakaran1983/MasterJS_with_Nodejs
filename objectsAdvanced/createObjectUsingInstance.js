64b256b6e64e26f2c95a525cc98d72fcde646129
Karunakaran Dhayanidhi <karunaitsurlife@gmail.com>
2024-07-06 10:38:40 +0200

    create object using Instance

---
+++ objectsAdvanced/createObjectUsingInstance.js
@@ -0,0 +1,14 @@
+/* 
+    Creating Objects using Instance
+*/
+
+const car = new Object({
+    name: "Honda",
+    model: "CRV",
+    year: 2022,
+    color: "White"
+})
+
+console.log(car)
+console.log(car.name)
+console.log(car["name"])
