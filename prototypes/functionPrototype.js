1e82bd6f754fabd26b9972aedb189c0e447bcc65
Karunakaran Dhayanidhi <karunaitsurlife@gmail.com>
2024-07-06 13:04:03 +0200

    function prototype

---
+++ prototypes/functionPrototype.js
@@ -0,0 +1,8 @@
+
+function kbc() {
+
+}
+
+console.log(kbc.__proto__); // {}
+console.log(kbc.__proto__.__proto__); // [Object: null prototype] {}
+console.log(kbc.__proto__.__proto__.__proto__); null
