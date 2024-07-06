9862ffc6adc553a6edd8a80a634caaf3b15523d0
Karunakaran Dhayanidhi <karunaitsurlife@gmail.com>
2024-07-06 21:10:01 +0200

    arrow fn with no  arg

---
+++ arrowFunctions/arrowFunctionWithNoArg.js
@@ -0,0 +1,12 @@
+// Traditional function
+
+let greet = function() {
+    console.log("Hello Traditional Fn");
+}
+
+// arrow fn with no args
+
+let greet1 = () => console.log("Hello Arrow Fn");
+
+greet()
+greet1()
