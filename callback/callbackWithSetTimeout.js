69cd866ed9c853292eb55f671e21329a9438cc33
Karunakaran Dhayanidhi <karunaitsurlife@gmail.com>
2024-07-07 16:58:52 +0200

    callback with settimeout

---
+++ callback/callbackWithSetTimeout.js
@@ -0,0 +1,16 @@
+/* 
+    The SetTimeout function allows you to schedule a one-time execution of a 
+    function after a specified number of milliseconds.
+*/
+
+setTimeout(fun1, 5000, 2450)
+
+function fun1(x) {
+    console.log(x);
+}
+
+setTimeout(() => {
+    console.log("Fun2 Executed");
+}, 6000);
+
+console.log("Rest of the program");
