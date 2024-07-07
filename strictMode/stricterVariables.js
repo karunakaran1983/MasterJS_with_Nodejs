f5efc74ba3ab9d4c3aa04e9736c2f98e78092ec9
Karunakaran Dhayanidhi <karunaitsurlife@gmail.com>
2024-07-07 10:35:04 +0200

    use strict

---
+++ strictMode/stricterVariables.js
@@ -0,0 +1,16 @@
+"use strict";
+
+// BEFORE USING STRICT
+// a = 100
+// undefined = 200
+
+// console.log(a); // 100
+// console.log(undefined); // undefined
+
+
+
+b = 100 // ReferenceError: b is not defined
+undefined = 201 // TypeError: Cannot assign to read only property 'undefined' of object '#<Object>'
+
+// console.log(b); 
+// console.log(undefined); 
