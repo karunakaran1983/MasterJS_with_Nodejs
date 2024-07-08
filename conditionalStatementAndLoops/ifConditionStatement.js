e804860eacf17dbbd65f14434c8249470137662f
Karunakaran Dhayanidhi <karunaitsurlife@gmail.com>
2024-07-08 07:12:56 +0200

    if condition

---
+++ conditionalStatementAndLoops/ifConditionStatement.js
@@ -0,0 +1,14 @@
+// Find greatest number among below 3 variables
+let a = 1000
+let b = 300
+let c = 500
+
+if (a > b && a > c){
+    console.log(`a ${a} is the greatest`)
+}else if(b > c){
+    console.log(`b ${b} is the greatest`);
+} else {
+    console.log(`c ${c} is the greatest`);
+}
+
+console.log("Done");
