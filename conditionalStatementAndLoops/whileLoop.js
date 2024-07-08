4a7ca8cabe42b75976648409360734d05e23ab30
Karunakaran Dhayanidhi <karunaitsurlife@gmail.com>
2024-07-08 09:04:11 +0200

    while loop

---
+++ conditionalStatementAndLoops/whileLoop.js
@@ -0,0 +1,15 @@
+
+let a = 100
+
+// // while loop
+// while(a >= 10) {
+//     console.log("Value of a > 10", a);
+//     a-=10
+// }
+
+// // do while
+a = 10
+do{
+    console.log("Value of a > 10 ", a);
+    a-=10
+}while(a>10)
