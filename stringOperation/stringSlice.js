698c326c0955271117ffb7ee34f9c05d0e78c1df
Karunakaran Dhayanidhi <karunaitsurlife@gmail.com>
2024-07-08 10:32:27 +0200

    slice

---
+++ stringOperation/stringSlice.js
@@ -0,0 +1,8 @@
+let str1 = "ABCDEFGHIJKLKMNOPQRSTUVWXYZ"
+// index of the start of the string starts with 0
+// index of the end of the string starts with -1
+
+console.log(str1.slice(3));
+console.log(str1.slice(3,6));
+console.log(str1.slice(-3));
+console.log(str1.slice(-6, -3));
