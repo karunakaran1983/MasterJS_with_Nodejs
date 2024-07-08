98c39e67e73032526cebfbde9f50090cbe354db4
Karunakaran Dhayanidhi <karunaitsurlife@gmail.com>
2024-07-08 09:49:38 +0200

    string start end

---
+++ stringOperation/stringStartAndEnd.js
@@ -0,0 +1,8 @@
+// startWith
+
+let str1 = "The way to get started is to quit talking and begin doing"
+let str2 = "The way"
+let str3 = "begin doing"
+
+console.log("startsWith => ", str1.startsWith(str2));
+console.log("endsWith   => ", str1.endsWith(str3));
