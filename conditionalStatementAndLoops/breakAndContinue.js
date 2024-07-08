5ec7ddb82309b19346ec9dfe173ebea74c86ecd7
Karunakaran Dhayanidhi <karunaitsurlife@gmail.com>
2024-07-08 09:14:49 +0200

    break continue

---
+++ conditionalStatementAndLoops/breakAndContinue.js
@@ -0,0 +1,18 @@
+// let a = 10
+
+// while(a < 100) {
+//     console.log(a);
+//     a+=10
+//     if(a == 50) {
+//         break
+//     }
+// }
+
+let b = 10
+while(b < 100) {
+    b+=10
+    if(b == 50) {
+        continue
+    }
+    console.log(b);
+}
