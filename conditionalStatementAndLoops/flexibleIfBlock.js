b8d24458fbe0df8a1e081e11240610039ac4f681
Karunakaran Dhayanidhi <karunaitsurlife@gmail.com>
2024-07-08 09:18:01 +0200

    flexible if block

---
+++ conditionalStatementAndLoops/flexibleIfBlock.js
@@ -0,0 +1,10 @@
+let val = ""
+
+// val is TRUE for number/string/true boolean
+// val is FALSE for null, undefined, false boolean, empty string, 0
+
+if(val) {
+    console.log("This is True");
+} else {
+    console.log("This is False");
+}
